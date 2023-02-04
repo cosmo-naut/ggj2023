09db2f030c7b4d843eea6d9a976f7afc5321c2aa
Bon Chau <bam1a@outlook.com>
2023-02-04 16:00:47 +1100

    generated buttons can be accessed their variables

--- game/ui_handler.cs
+++ game/ui_handler.cs
@@ -9,6 +9,7 @@ public struct Action
     public int cost;
     public string name;
 
+    //properties of the each action button
     public Action(int cost, string name)
     {
         this.cost = cost;
@@ -22,26 +23,34 @@ public class ui_handler : Node
     [Signal]
     public delegate void ActionClicked(string actionName);
 
+    PackedScene actionButtonTemp;//the button's base to be instanced
+    Node buttonContainer;//container of the buttons.
 
-    Node buttonContainer;
-
-
+    //List of action buttons to be generated and its parameters
     List<Action> actions = new List<Action>{
         new Action(1, "Grow tentacle"),
         new Action(3, "Grow clot"),
         new Action(5, "Eye")
     };
+    List <ActionButton>buttonList=new List<ActionButton>();
+
 
 
     public override void _Ready()
     {
+        //ready for the custom button "template"
+        actionButtonTemp = (PackedScene)GD.Load("res://ActionButton.tscn");
         buttonContainer = GetNode("ButtonContainer");
         RenderButtons();
     }
 
-    void _ButtonClicked()
+    void _ButtonClicked(ActionButton targetButton)
     {
-        GD.Print("whatever");
+        
+        GD.Print(targetButton.buttonName+" "+targetButton.cost);
+        if(targetButton.buttonName=="Grow tentacle")
+            GD.Print("It Grows!!!!!");
+        
     }
 
 
@@ -53,14 +62,19 @@ public class ui_handler : Node
         var buttonYPosition = 50;
 
         for (var i = 0; i < actions.Count; i++)
-        {
+        {                        
             var action = actions[i];
-            var button = new Button();
-            button.Text = action.name;
+            //create a new instance of action button
+            ActionButton button= (ActionButton)actionButtonTemp.Instance();
             button.RectMinSize = new Vector2(150, 30);
+            button.Text = action.name;
+            button.buttonName = action.name;
+            button.cost = action.cost;
+            GD.Print(button.buttonName);
 
-            button.Connect("pressed", this, "_ButtonClicked");
-            // button.SetPosition(new Vector2(buttonSpacing * i, buttonYPosition));
+            buttonList.Add(button);
+            button.Connect("pressed", this, "_ButtonClicked", new Godot.Collections.Array(){button});
+            //button.SetPosition(new Vector2(buttonSpacing * i, buttonYPosition));
 
             buttonContainer.AddChild(button);
         }
