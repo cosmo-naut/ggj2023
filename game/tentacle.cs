65ff4415f97887a7592063d601d0b91ebfa689c5
Ron <ronaldchau14@gmail.com>
2023-02-04 14:00:52 +1100

    Tentacle growth limit added

--- game/tentacle.cs
+++ game/tentacle.cs
@@ -6,9 +6,15 @@ public class tentacle : Node2D
 {
   private Vector2 headPosition;
   [Export] public float SpawnDistance = 50.0f;
-  [Export] public float growthSpeed = 100.0f;
+  [Export] public float Speed = 100.0f;
+
+  [Export] public float growthDistance = 1000.0f;
+  [Export] public float growthRate = 40.0f;
+  private float growthCapacity;
+
   private List<Vector2> points;
   private Vector2 lastPosition;
+  private bool activeTentacle;
 
   public override void _Ready()
   {
@@ -16,6 +22,9 @@ public class tentacle : Node2D
     headPosition = Position;
     points.Add(headPosition);
     lastPosition = headPosition;
+
+    growthCapacity = growthDistance;
+    activeTentacle = true;
   }
 
   public override void _Draw()
@@ -32,35 +41,57 @@ public class tentacle : Node2D
 
   public override void _Process(float delta)
   {
-    Vector2 dir = Vector2.Zero;
+    if (activeTentacle)
+    {
+      Vector2 dir = Vector2.Zero;
+      if (Input.IsActionPressed("ui_right"))
+      {
+        dir.x += 1.0f;
+      }
+      if (Input.IsActionPressed("ui_left"))
+      {
+        dir.x -= 1.0f;
+      }
+      if (Input.IsActionPressed("ui_down"))
+      {
+        dir.y += 1.0f;
+      }
+      if (Input.IsActionPressed("ui_up"))
+      {
+        dir.y -= 1.0f;
+      }
 
-    if (Input.IsActionPressed("ui_right"))
-    {
-      dir.x += 1.0f;
-    }
-    if (Input.IsActionPressed("ui_left"))
-    {
-      dir.x -= 1.0f;
-    }
-    if (Input.IsActionPressed("ui_down"))
-    {
-      dir.y += 1.0f;
-    }
-    if (Input.IsActionPressed("ui_up"))
-    {
-      dir.y -= 1.0f;
-    }
+      headPosition += dir.Normalized() * Speed * delta;
 
-    headPosition += dir.Normalized() * growthSpeed * delta;
+      // Spawn point
+      if (lastPosition.DistanceTo(headPosition) >= SpawnDistance)
+        AddTentacleNode(headPosition);
 
-    // Spawn point
-    if (lastPosition.DistanceTo(headPosition) >= SpawnDistance)
-    {
-      points.Add(headPosition);
-      lastPosition = headPosition;
-      GD.Print("Add point", points.Count);
+      // Check for movement
+      if (dir != Vector2.Zero)
+        updateGrowth();
+
+      Update();
     }
+  }
 
-    Update();
+  void updateGrowth()
+  {
+    growthCapacity -= growthRate;
+    growthCapacity = Math.Max(growthCapacity, 0.0f);
+
+    // Is done
+    activeTentacle = growthCapacity > 0.0f;
+
+    // Add last node
+    AddTentacleNode(headPosition);
+    GD.Print("Growth Capacity {sdf}", (growthCapacity / growthDistance) * 100, "%");
+  }
+
+  void AddTentacleNode(Vector2 pos)
+  {
+    points.Add(pos);
+    lastPosition = pos;
+    GD.Print("Add point ", points.Count);
   }
 }
