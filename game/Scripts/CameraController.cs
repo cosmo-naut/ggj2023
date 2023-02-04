326e88e6332fc0f8a583deeca005dccaaba287ca
Louis Johnson <louis@repeat.gg>
2023-02-04 18:38:32 +1000

    formatting

--- game/Scripts/CameraController.cs
+++ game/Scripts/CameraController.cs
@@ -1,30 +1,18 @@
 using Godot;
-using System;
 
 public class CameraController : Camera2D
 {
-
-
     public float movementSpeed = 250f;
-
 
     float zoomSpeed = 0.85f;
     float maxZoom = 2.5f;
     float minZoom = 0.5f;
 
 
-
-
-
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Current = true;
-
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -62,15 +50,12 @@ public class CameraController : Camera2D
             zoomThisFrame -= zoomSpeed * delta;
         }
 
-
-
         var zoomClamp = Mathf.Clamp(zoomThisFrame + Zoom.x, minZoom, maxZoom);
 
         Zoom = new Vector2(zoomClamp, zoomClamp);
 
         // Normalize movement
         toMove = toMove.Normalized();
-
 
         Translate(toMove * delta * movementSpeed);
     }
