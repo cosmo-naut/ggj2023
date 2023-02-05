499b28670d3331c4e91b3d99c4e869e84718e888
Ron <ronaldchau14@gmail.com>
2023-02-05 14:26:40 +1100

    Change play scene

--- game/scripts/TitleScreen.cs
+++ game/scripts/TitleScreen.cs
@@ -1,15 +1,8 @@
 using Godot;
 using System;
 
-
-
 public class TitleScreen : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
     PackedScene playScene=null;//container for the main game scene.
     PackedScene howToScene=null;//for the game description/instruction scene(or credits???)
 
@@ -23,36 +16,24 @@ public class TitleScreen : Control
         GetNode<VBoxContainer>("MainButtonContainer").GetNode<TextureButton>("QuitButton").Connect("pressed", this, "_QuitButtonClick");
 
         //preload the gameplay scene
-        playScene = (PackedScene)ResourceLoader.Load("res://scenes/main.tscn");
+        playScene = (PackedScene)ResourceLoader.Load("res://scenes/Game.tscn");
         //preload the howto scene
         howToScene = (PackedScene)ResourceLoader.Load("res://scenes/HowToScreen.tscn");
 
     }
+
     public void _QuitButtonClick()
     {
         GetTree().Quit(); 
-        //GD.Print("quitting?");
     }
+    
     public void _PlayButtonClick()
     {
-        //GD.Print("playing?");
-        //load the new level
         GetTree().ChangeSceneTo(playScene);
     }
+    
     public void _HowToButtonClick()
     {
-        GD.Print("how to play?");
         GetTree().ChangeSceneTo(howToScene);
-
     }
-
-
-
-
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
