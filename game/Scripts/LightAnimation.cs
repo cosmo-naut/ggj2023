20c31a14b975bb198b41e1bdff862d493f212f00
Louis Johnson <louis@repeat.gg>
2023-02-05 10:28:33 +1000

    added light animation for tentacles

---
+++ game/Scripts/LightAnimation.cs
@@ -0,0 +1,35 @@
+using Godot;
+
+
+public class LightAnimation : Light2D
+{
+
+
+    [Export]
+    float animateAmount = 0.02f;
+
+    [Export]
+    float animationTimerAmount = 0.05f;
+
+
+    float timer = 0;
+
+    Vector2 originalScale;
+
+    public override void _Ready()
+    {
+        originalScale = Scale;
+    }
+
+
+    public override void _Process(float delta)
+    {
+
+        timer += 1;
+
+        float newScale = Mathf.Sin(timer * animationTimerAmount) * animateAmount;
+
+        Scale = new Vector2(originalScale.x + newScale, originalScale.y + newScale);
+    }
+
+}
